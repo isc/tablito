9863a8d84057d7b828e9af16c74eb6a2a7e60f6a
isc <isc@users.noreply.github.com>
2026-05-13 19:12:43 +0000

    deploy: main @ 2e3c97739016996162cc34ab4c16f3da4b6a561e 2e3c97739016996162cc34ab4c16f3da4b6a561e

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
 // Les marqueurs de version, de base path et de liste d'assets sont
 // substitués par scripts/build.mjs.
 
-const CACHE = 'multiplix-' + "20260513185809"
+const CACHE = 'multiplix-' + "20260513191155"
 const BASE = "/multiplix/"
 const ASSETS = [
   "/multiplix/favicon.svg",
