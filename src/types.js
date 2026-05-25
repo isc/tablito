474b99c0f3d9260af93e28385df7476a351e40ae
isc <isc@users.noreply.github.com>
2026-05-25 17:51:16 +0000

    deploy: main @ 32c74e545d69900ebc8433298926ce2309633d50 32c74e545d69900ebc8433298926ce2309633d50

--- src/types.js
+++ src/types.js
@@ -1,3 +1,3 @@
-const e=["market","ocean"],t={1:0,2:1,3:3,4:7,5:21},r={FAST:3e3,SLOW:5e3},n={PREMIER_PAS:"premier-pas",PREMIERE_CASE:"premiere-case",PREMIERE_MAITRISE:"premiere-maitrise",REGULIER:"regulier",MACHINE:"machine",EXPLORATION:"exploration",TABLE_PREFIX:"table-",GENIE_MATHS:"genie-maths",VELOCE:"veloce",PERSEVERANCE:"perseverance",FLAMME_ETERNELLE:"flamme-eternelle"};export{n as BADGE_IDS,t as BOX_INTERVALS,e as MYSTERY_POOL,r as RESPONSE_TIME};
+const e=["market","ocean"],t={1:0,2:1,3:3,4:7,5:21},n={FAST:3e3,SLOW:5e3},r={PREMIER_PAS:"premier-pas",PREMIERE_CASE:"premiere-case",PREMIERE_MAITRISE:"premiere-maitrise",REGULIER:"regulier",MACHINE:"machine",EXPLORATION:"exploration",TABLE_PREFIX:"table-",GENIE_MATHS:"genie-maths",VELOCE:"veloce",PERSEVERANCE:"perseverance",FLAMME_ETERNELLE:"flamme-eternelle"};export{r as BADGE_IDS,t as BOX_INTERVALS,e as MYSTERY_POOL,n as RESPONSE_TIME};
 
 //# sourceMappingURL=types.js.map
