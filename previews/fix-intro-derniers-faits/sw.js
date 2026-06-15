a8e39c164db311a503ec892f1e709d5bbd09d81e
isc <isc@users.noreply.github.com>
2026-06-15 11:20:35 +0000

    preview: fix-intro-derniers-faits @ 9efad4b275b5094f3a76e5365184fb1131f1ff49 2d9e6f55c6048924af6fc41c56d20ef1d9ce92de

--- previews/fix-intro-derniers-faits/sw.js
+++ previews/fix-intro-derniers-faits/sw.js
@@ -26,7 +26,7 @@
 // Les marqueurs de version, de base path et de liste d'assets sont
 // substitués par scripts/build.mjs.
 
-const CACHE = 'tablito-' + "20260615074121"
+const CACHE = 'tablito-' + "20260615111936"
 const BASE = "/previews/fix-intro-derniers-faits/"
 const ASSETS = [
   "/previews/fix-intro-derniers-faits/CNAME",
