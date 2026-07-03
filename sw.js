bc20dd3a27f78a87b667489a2cc6db82d1314a3e
isc <isc@users.noreply.github.com>
2026-07-03 12:45:08 +0000

    deploy: main @ 733120d53ef4abe1e479434c115d74fd51d8b387 733120d53ef4abe1e479434c115d74fd51d8b387

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@
 // Les marqueurs de version, de base path et de liste d'assets sont
 // substitués par scripts/build.mjs.
 
-const CACHE = 'tablito-' + "20260703123046"
+const CACHE = 'tablito-' + "20260703124346"
 const BASE = "/"
 const ASSETS = [
   "/CNAME",
@@ -149,6 +149,7 @@ const ASSETS = [
   "/src/lib/supabase.js",
   "/src/lib/transfer.js",
   "/src/lib/utils.js",
+  "/src/lib/voiceDebug.js",
   "/src/main.js",
   "/src/screens/BadgesScreen.js",
   "/src/screens/ChangelogScreen.js",
