a24c4e3a018de76336504e2e575b1d5f510fd5ca
isc <isc@users.noreply.github.com>
2026-07-22 12:41:55 +0000

    deploy: main @ ab2f9c5be452540dca45b2fef83d05a620ffa90b ab2f9c5be452540dca45b2fef83d05a620ffa90b

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@
 // Les marqueurs de version, de base path et de liste d'assets sont
 // substitués par scripts/build.mjs.
 
-const CACHE = 'tablito-' + "20260721103012"
+const CACHE = 'tablito-' + "20260722124033"
 const BASE = "/"
 const ASSETS = [
   "/CNAME",
