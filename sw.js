37d36d3b5496c4e766759e492da6b46142b5f35b
isc <isc@users.noreply.github.com>
2026-09-23 18:30:26 +0000

    deploy: main @ f06ce90944207b1a6bff30468089a87cee31ea7d f06ce90944207b1a6bff30468089a87cee31ea7d

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@
 // Les marqueurs de version, de base path et de liste d'assets sont
 // substitués par scripts/build.mjs.
 
-const CACHE = 'tablito-' + "20260923065645"
+const CACHE = 'tablito-' + "20260923182853"
 const BASE = "/"
 const ASSETS = [
   "/favicon.svg",
